bd7444232795786d5c545162fa42f1d933f85f0e
Tyler Cako <>
2025-01-01 17:44:40 -0700

    .

--- webscraper/crawler/Crawler.py
+++ webscraper/crawler/Crawler.py
@@ -8,29 +8,32 @@ class Crawler(object):
     def __init__(self):
         self.to_visit = []
         self.visited = set()
-        self.pasrer = HTMLParser()
+        self.parser = HTMLParser()
         self.robotParser = robotparser.RobotFileParser()
 
-    def canCrawl(self, url:str) -> True:
+    def canCrawl(self, url: str) -> bool:
         """Check Robots.txt to see if we can crawl this URL"""
 
-        return self.robotParser.can_fetch("*", url)
+        print("enter canCrawl")
+        can_fetch = self.robotParser.can_fetch("*", url)
+        print(can_fetch)
+        return True
         # root_url = urlparse(url).hostname
         # robot_url = root_url + "/robots.txt"
         # self.robotParser.set_url(robot_url)
-    
+
     def crawl(self, path: str) -> None:
         """Start URL crawling given file path to a list of urls"""
-        
+
         file = open(path)
-        
+
         url = file.readline()
         # Read list of files from txt document
         while url:
             print(f"url: {url}")
             self.crawlUrl(url, 10)
             url = file.readline()
-        
+
         file.close()
 
     def crawlUrl(self, url: str, counter: int) -> None:
@@ -38,13 +41,13 @@ class Crawler(object):
 
         if url in self.visited:
             return
-        
+
         #Check robots.txt
         if not self.canCrawl(url):
             return
-        
+
         #Parse URL
-        url_list = [] # New URLs to crawl 
+        url_list = self.parser.parse(url) # New URLs to crawl
 
         self.visited.add(url)
 
--- webscraper/crawler/parser/parser.py
+++ webscraper/crawler/parser/parser.py
@@ -1,5 +1,36 @@
 from html.parser import HTMLParser
+from urllib.request import urlopen
+from typing import List
 
 class HTMLParser(HTMLParser):
-    def parse(url: str):
-        print(url)
+    def handle_starttag(self, tag, attrs):
+        """Handle <a> tags to continue crawling"""
+
+
+    def parse(self, url: str) -> List[str]:
+        """Parse a given URL's page. Returns list of links to crawl next"""
+
+        print("test")
+
+        self.url_list = []
+
+        try:
+            print("Enter HTMLParser: run")
+            # urlopen returns served html as a bytearray when read, need to decode
+            response = urlopen(url)
+
+            # First check if charset is utf8
+            content_charset = response.headers.get_content_charset()
+
+            if content_charset != "utf8":
+                raise Exception("HTMLParser parse error. Page not in utf8 encoding!")
+
+            responseHtml = response.read().decode('utf8')
+            if responseHtml:
+                print("Page received.")
+                self.feed(responseHtml)
+        except Exception as inst:
+            print(f"parsing error: {inst}")
+
+
+        return self.url_list
